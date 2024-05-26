4e5c626ba6849bb53c63ccf77119686370f20e9c
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-05-26 22:47:41 +0300

    Add files via upload

---
+++ homework2-3.py
@@ -0,0 +1,38 @@
+my_list1 = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list2 = [0]
+while True:
+    if my_list1 > my_list2:
+        print(42)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(69)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(322)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(13)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(99)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(9)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(8)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(7)
+        break
+while True:
+    if my_list1 > my_list2:
+        print(5)
+        break
